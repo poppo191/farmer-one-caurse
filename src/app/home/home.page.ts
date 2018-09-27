ad3a5623edcd64508427f1ce5d127013975e71fd
Sittikorn Asavathavornvanich <popswit@gmail.com>
2018-09-27 12:18:39 +0700

    Update login code

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,29 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage {}
+export class HomePage {
+
+  constructor(
+    private httpClient: HttpClient
+  ) {
+    this.ionViewDidLoad();
+  }
+
+  ionViewDidLoad() {
+    const login = {
+      id: 'train01',
+      password: 'oaetrain123'
+    };
+    const body = JSON.stringify(login);
+    const path = 'http://opendata.oae.go.th:8080/farmer/api/v1/authentication';
+    this.httpClient.post(path, body)
+      .subscribe((response) => {
+        console.log('response : ', response);
+      });
+  }
+}
