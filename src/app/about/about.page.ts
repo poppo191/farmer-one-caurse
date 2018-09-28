7b1d7135dc9102b9f4a497ebd5499626c44f45f6
Sittikorn Asavathavornvanich <popswit@gmail.com>
2018-09-28 11:20:29 +0700

    Line chart

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -10,9 +10,11 @@ export class AboutPage implements AfterContentInit {
 
   @ViewChild('barCanvas') barCanvas;
   @ViewChild('doughnutCanvas') doughnutCanvas;
+  @ViewChild('lineCanvas') lineCanvas;
 
   barChart: any;
   doughnutChart: any;
+  lineChart: any;
 
   ngAfterContentInit() {
     this.barChart = new Chart(this.barCanvas.nativeElement, {
@@ -55,7 +57,6 @@ export class AboutPage implements AfterContentInit {
     });
 
     this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
-
       type: 'doughnut',
       data: {
         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
@@ -80,8 +81,40 @@ export class AboutPage implements AfterContentInit {
           ]
         }]
       }
-
     });
+
+    this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+
+      type: 'line',
+      data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+          {
+            label: 'My First dataset',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40],
+            spanGaps: false,
+          }
+        ]
+      }
+    });
+
 
   }
 
